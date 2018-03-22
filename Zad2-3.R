2ace2bd54fd8b61415a56ce4e6489d82475ca9ea
Aleksandra <a.zamarja@gmail.com>
2018-03-22 20:03:49 +0100

    Zadanka

--- Zad2-3.R
+++
@@ -1,8 +0,0 @@
-rok <- (1800:2018)
-for(rok_przystepny in rok) {
- if (rok_przystepny == rok %% 4 & rok %% 400) {
-   print ("To jest rok przystepny")
- } else {
-   print ("To nie jest rok przystepny")
- } 
-}
